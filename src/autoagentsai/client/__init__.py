ef67d50b89a37fc5a1d270cb7fe0495d3b5380ab
Hehua Fan <forhheart5532@gmail.com>
2025-07-26 22:32:28 +0800

    v0.0.9: add supabase

--- src/autoagentsai/client/__init__.py
+++ src/autoagentsai/client/__init__.py
@@ -1,8 +1,9 @@
 from .ChatClient import ChatClient
 from .KbClient import KbClient
 from .MCPClient import MCPClient
+from .SupabaseClient import SupabaseClient
 
-__all__ = ["ChatClient", "KbClient", "MCPClient"]
+__all__ = ["ChatClient", "KbClient", "MCPClient", "SupabaseClient"]
 
 
 def main() -> None:
